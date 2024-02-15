6e126f8ff29327ef5c0bb670c992982e25f36fc3
huefnerpaul <huefner.paul@gmail.com>
2024-02-15 22:48:48 +0100

    Added Drag & Drop Zone

--- InterShareWindows/Views/MainPage.xaml.cs
+++ InterShareWindows/Views/MainPage.xaml.cs
@@ -1,5 +1,10 @@
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI;
 using InterShareWindows.ViewModels;
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 
 namespace InterShareWindows.Views;
 
@@ -15,5 +20,22 @@ public sealed partial class MainPage : Page
         ViewModel = App.GetService<MainViewModel>();
         DataContext = ViewModel;
         InitializeComponent();
+    }
+    private void OnDragOver(object sender, DragEventArgs eventArgs)
+    {
+        eventArgs.AcceptedOperation = DataPackageOperation.Copy;
+        DropZone.Background = new SolidColorBrush(Color.FromArgb(200, 80, 91, 250));
+    }
+
+    private void OnDragLeave(object sender, DragEventArgs eventArgs)
+    {
+        eventArgs.AcceptedOperation = DataPackageOperation.Copy;
+        DropZone.Background = new SolidColorBrush(Colors.Transparent);
+    }
+
+    private void OnDrop(object sender, DragEventArgs eventArgs)
+    {
+        eventArgs.AcceptedOperation = DataPackageOperation.Copy;
+        DropZone.Background = new SolidColorBrush(Colors.Transparent);
     }
 }
