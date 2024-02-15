de7bb685cd5aca8edea97eee021aae008e720952
Jakob Bauer <mail@bauer-jakob.de>
2024-02-15 23:17:26 +0100

    Implemented file picker when sending file

--- InterShareWindows/ViewModels/MainViewModel.cs
+++ InterShareWindows/ViewModels/MainViewModel.cs
@@ -1,12 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Storage.Pickers;
+using Windows.UI.Core;
+using ABI.System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 using InterShareWindows.Services;
+using Microsoft.UI.Xaml;
+using WinUIEx;
 
 namespace InterShareWindows.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
+    public readonly AsyncRelayCommand SendFileCommand;
+    
     public MainViewModel(ITestService testService)
     {
         Name = testService.GetName();
+
+        SendFileCommand = new AsyncRelayCommand(SendFileAsync);
+    }
+
+    private async Task SendFileAsync()
+    {
+        var picker = new FileOpenPicker();
+        
+        var hwnd = App.MainWindow.GetWindowHandle();
+        WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
+        
+        picker.FileTypeFilter.Add("*");
+        picker.FileTypeFilter.Add(".jpg");
+        picker.FileTypeFilter.Add(".jpeg");
+        picker.FileTypeFilter.Add(".png");
+        picker.FileTypeFilter.Add(".svg");
+        
+        var file = await picker.PickSingleFileAsync();
     }
 
     public string Name { get; set; }
--- InterShareWindows/Views/MainPage.xaml.cs
+++ InterShareWindows/Views/MainPage.xaml.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using InterShareWindows.ViewModels;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using WinUIEx;
 
 namespace InterShareWindows.Views;
 
@@ -15,5 +21,10 @@ public sealed partial class MainPage : Page
         ViewModel = App.GetService<MainViewModel>();
         DataContext = ViewModel;
         InitializeComponent();
+    }
+
+    private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        await Task.CompletedTask;
     }
 }
