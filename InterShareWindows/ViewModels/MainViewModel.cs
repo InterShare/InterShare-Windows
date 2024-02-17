58565bb47603e0d7c8526e4f0ced147370ad4cd6
Paul Hüfner <paul.huefner@wanko.de>
2024-02-17 16:21:15 +0100

    Added Send Clipboard Button

--- InterShareWindows/ViewModels/MainViewModel.cs
+++ InterShareWindows/ViewModels/MainViewModel.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Diagnostics;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage.Pickers;
 using Windows.UI.Core;
+using Windows.UI.Popups;
 using ABI.System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
 using InterShareWindows.Services;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using WinUIEx;
 
 namespace InterShareWindows.ViewModels;
@@ -15,12 +20,14 @@ public class MainViewModel : ViewModelBase
 {
     private readonly INavigationService _navigationService;
     public readonly AsyncRelayCommand SendFileCommand;
+    public readonly AsyncRelayCommand SendClipCommand;
     public readonly RelayCommand OpenSettingsPageCommand;
     
     public MainViewModel(INavigationService navigationService)
     {
         _navigationService = navigationService;
         SendFileCommand = new AsyncRelayCommand(SendFileAsync);
+        SendClipCommand = new AsyncRelayCommand(SendClipAsync);
         OpenSettingsPageCommand = new RelayCommand(OpenSettingsPage);
     }
 
@@ -43,5 +50,19 @@ public class MainViewModel : ViewModelBase
         picker.FileTypeFilter.Add(".svg");
         
         var file = await picker.PickSingleFileAsync();
+    }
+
+    private async Task SendClipAsync()
+    {
+        DataPackageView clipContent = Clipboard.GetContent();
+
+        if (clipContent.Contains(StandardDataFormats.Text))
+        {
+            string text = await clipContent.GetTextAsync();
+        }
+        else
+        {
+            //Todo: If clipContent is is a file, the file should be handled as selected via sendFile
+        }
     }
 }
